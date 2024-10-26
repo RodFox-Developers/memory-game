58311cc46eea743075e1e6815ce710c3282ff931
Rodrigo Cevas <rodrigo.cevas@in2.es>
2024-10-26 15:48:15 +0200

    single card

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,43 @@
+import { useState } from 'react'
 import './App.css'
+import SingleCard from './components/SingleCard';
+
+const cardImages = [
+  {"src": "/src/assets/helmet-1.png"},
+  {"src": "/src/assets/potion-1.png"},
+  {"src": "/src/assets/ring-1.png"},
+  {"src": "/src/assets/scroll-1.png"},
+  {"src": "/src/assets/shield-1.png"},
+  {"src": "/src/assets/sword-1.png"}
+]
 
 function App() {
 
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }))
+
+      setCards(shuffledCards);
+      setTurns(0);
+  }
+
+  console.log({cards});
+  console.log({turns});
 
   return (
     <div>
       <h1>Magic Match</h1>
-      <button>New Game</button>
+      <button onClick={shuffleCards}>New Game</button>
+
+      <div className='card-grid'>
+        {cards.map(card => (
+          <SingleCard key={card.id} card={card} />
+        ))}
+      </div>
     </div>
   )
 }
